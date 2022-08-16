7d2c0da91e15e22afa8448d08c815447da8c5b0e
cheeter_Lee <johnson991210@gmail.com>
2022-08-16 17:39:47 +0800

    add css and js

---
+++ scripts/forecast.js
@@ -0,0 +1,29 @@
+const key = 'c86x8Bkav3rHOtyo7oYCehHuZ0kdGBUa'
+
+// get weather information
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    const query = `${id}?apikey=${key}`
+
+    const response = await fetch(base + query)
+    const data = await response.json()
+
+    return data[0]
+
+}
+
+// get city information 
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    const query = `?apikey=${key}&q=${city}`
+
+    const response = await fetch(base + query)
+    const data = await response.json()
+
+    return (data[0])
+}
+
+
+
